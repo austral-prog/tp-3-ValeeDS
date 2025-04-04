eadc4ef48399e8a4799c29908b9e1bc54bcb35a6
Valeria Del Savio <valeria.delsavio@hotmail.com>
2025-04-04 12:55:05 -0300

    correct slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -5,7 +5,6 @@ def slice_advanced():
     slicing = texto[4::2]
 
     print(slicing)
-
-slice_advanced()
+    
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
